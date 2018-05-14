6ab178bf90db57db40b5505044eb8aae1ccf14b2
Eoin O'Brien <eoinobrien910@gmail.com>
2018-05-14 22:57:05 +0100

    feat(api): add roomDetails() API function

--- __tests__/room-details.spec.ts
+++ __tests__/room-details.spec.ts
@@ -1,11 +1,45 @@
 import { roomDetails } from '../src';
+import { IRoomDetails } from '../src/api/room-details';
+import { buildings, floors } from '../src/util/constants';
+import { isValidRoomId } from '../src/util/validators';
 
-const roomDetailsURI = 'https://www.timetable.ul.ie/room_res.asp';
+jest.mock('../src/util/validators');
 
 describe('roomDetails()', () => {
-  it('parses the room details from the room ID');
+  const invalidRoomId = '42';
 
-  it('throws an error if the room ID is invalid');
+  afterEach(() => {
+    (<jest.Mock>isValidRoomId).mockReset();
+  });
 
-  it('throws an error if the request fails');
+  it('parses the room details from the room ID', async () => {
+    (<jest.Mock>isValidRoomId).mockReturnValue(true);
+
+    // Samples of valid suffixes for room codes
+    const suffixes = ['001', '24A', '1', '01'];
+
+    // Test every valid combination of building, floor and suffix
+    for (const building of Object.keys(buildings)) {
+      for (const floor of floors) {
+        for (const suffix of suffixes) {
+          const mockRoomDetails: IRoomDetails = {
+            id: `${building}${floor}${suffix}`,
+            buildingName: buildings[building],
+            buildingCode: building,
+            floor: floor,
+            room: suffix,
+          };
+          await expect(roomDetails(`${building}${floor}${suffix}`)).resolves.toEqual(mockRoomDetails);
+          expect(isValidRoomId).lastCalledWith(`${building}${floor}${suffix}`);
+        }
+      }
+    }
+  });
+
+  it('throws an error if the room ID is invalid', async () => {
+    (<jest.Mock>isValidRoomId).mockReturnValueOnce(false);
+
+    await expect(roomDetails(invalidRoomId)).rejects.toEqual(new Error(`invalid room ID "${invalidRoomId}"`));
+    expect(isValidRoomId).toBeCalledWith(invalidRoomId);
+  });
 });
--- src/api/room-details.ts
+++ src/api/room-details.ts
@@ -1,5 +1,27 @@
-const roomDetailsURI = 'https://www.timetable.ul.ie/room_res.asp';
+import { buildings, floors } from '../util/constants';
+import { isValidRoomId } from '../util/validators';
 
-export async function roomDetails() {
+export interface IRoomDetails {
+  id: string;
+  buildingName: string;
+  buildingCode: string;
+  floor: string;
+  room: string;
+}
 
+export async function roomDetails(roomId: string): Promise<IRoomDetails> {
+  if (!isValidRoomId(roomId)) {
+    throw new Error(`invalid room ID "${roomId}"`);
+  }
+
+  const roomIdRegex = new RegExp(`^(${Object.keys(buildings).join('|')})(${floors.join('|')})([0-9]+[A-Z]?)$`);
+  const roomIdParts = roomIdRegex.exec(roomId.toUpperCase());
+
+  return {
+    id: roomId,
+    buildingName: buildings[roomIdParts[1]],
+    buildingCode: roomIdParts[1],
+    floor: roomIdParts[2],
+    room: roomIdParts[3],
+  };
 }
