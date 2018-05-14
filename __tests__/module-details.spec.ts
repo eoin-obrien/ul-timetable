5f6c86f53315815ab5d649e5566485a85845ad4b
Eoin O'Brien <eoinobrien910@gmail.com>
2018-05-14 18:46:36 +0100

    feat(api): add moduleDetails() API function

--- __tests__/module-details.spec.ts
+++ __tests__/module-details.spec.ts
@@ -1,11 +1,53 @@
+import cheerio from 'cheerio';
+import fs from 'fs';
+
 import { moduleDetails } from '../src';
+import { IModuleDetails } from '../src/api/module-details';
+import { isValidModuleId } from '../src/util/validators';
+import { webscraper } from '../src/util/webscraper';
 
 const moduleDetailsURI = 'https://www.timetable.ul.ie/tt_moduledetails_res.asp';
+const html = fs.readFileSync('__tests__/html/module-details.html');
+const $ = cheerio.load(html.toString());
+const mockModuleDetails: IModuleDetails = {
+  id: 'CS4006',
+  name: 'INTELLIGENT SYSTEMS',
+};
+
+jest.mock('../src/util/validators');
+jest.mock('../src/util/webscraper');
 
 describe('moduleDetails()', () => {
-  it('requests and parses the module details');
+  const moduleId = 'CS4006';
+  const invalidModuleId = 'apples';
 
-  it('throws an error if the module ID is invalid');
+  afterEach(() => {
+    (<jest.Mock>webscraper).mockReset();
+    (<jest.Mock>isValidModuleId).mockReset();
+  });
 
-  it('throws an error if the request fails');
+  it('requests and parses the module details', async () => {
+    (<jest.Mock>isValidModuleId).mockImplementationOnce(() => true);
+    (<jest.Mock>webscraper).mockImplementationOnce(async () => $);
+
+    await expect(moduleDetails(moduleId)).resolves.toEqual(mockModuleDetails);
+    expect(isValidModuleId).toBeCalledWith(moduleId);
+    expect(webscraper).toBeCalledWith(moduleDetailsURI);
+  });
+
+  it('throws an error if the module ID is invalid', async () => {
+    (<jest.Mock>isValidModuleId).mockImplementationOnce(() => false);
+
+    await expect(moduleDetails(invalidModuleId)).rejects.toEqual(new Error('invalid module ID'));
+    expect(isValidModuleId).toBeCalledWith(invalidModuleId);
+    expect(webscraper).not.toBeCalled();
+  });
+
+  it('throws an error if the request fails', async () => {
+    (<jest.Mock>isValidModuleId).mockImplementationOnce(() => true);
+    (<jest.Mock>webscraper).mockImplementationOnce(async () => { throw new Error(); });
+
+    await expect(moduleDetails(moduleId)).rejects.toEqual(new Error('failed to fetch module details from www.timetable.ul.ie'));
+    expect(webscraper).toBeCalledWith(moduleDetailsURI);
+  });
 });
--- src/api/module-details.ts
+++ src/api/module-details.ts
@@ -1,5 +1,37 @@
+import { isValidModuleId } from '../util/validators';
+import { webscraper } from '../util/webscraper';
+
+export interface IModuleDetails {
+  id: string;
+  name: string;
+}
+
 const moduleDetailsURI = 'https://www.timetable.ul.ie/tt_moduledetails_res.asp';
 
-export async function moduleDetails() {
+const idSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(2)';
+const nameSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(2) > td:nth-child(2)';
 
+export async function moduleDetails(moduleId: string): Promise<IModuleDetails> {
+  // Check that the module ID is valid
+  if (!isValidModuleId(moduleId)) {
+    throw new Error('invalid module ID');
+  }
+
+  // Scrape module details from www.timetable.ul.ie
+  let $: CheerioStatic;
+  try {
+    $ = await webscraper(moduleDetailsURI);
+  } catch (err) {
+    throw new Error('failed to fetch module details from www.timetable.ul.ie');
+  }
+
+  // Return parsed module details
+  return parseModuleDetails($);
+}
+
+export function parseModuleDetails($: CheerioStatic): IModuleDetails {
+  return {
+    id: $(idSelector).text().trim(),
+    name: $(nameSelector).text().trim(),
+  };
 }
