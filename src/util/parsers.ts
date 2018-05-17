845bade178746d4b88a2eb36d215e8b6c8229410
Eoin O'Brien <eoinobrien910@gmail.com>
2018-05-17 19:56:10 +0100

    feat(parsers): create module for parsing logic

---
+++ src/util/parsers.ts
@@ -0,0 +1,229 @@
+import {
+  Day,
+  ICourseTimetableLesson,
+  IModuleDetails,
+  IModuleExamTimetable,
+  IModuleTimetableLesson,
+  IRoomTimetableLesson,
+  IStudentTimetableLesson,
+  ITimetableLesson,
+  ITimetableLessons,
+  IWeekDetails,
+  LessonType,
+} from '../types';
+
+export function parseWeekDetails($: CheerioStatic): IWeekDetails[] {
+  const weekSelector = 'body > table > tbody > tr:not(:first-child)';
+  const startDateSelector = 'td:nth-child(1)';
+  const teachingWeekSelector = 'td:nth-child(2)';
+  const timetableWeekSelector = 'td:nth-child(3)';
+
+  const weeks: IWeekDetails[] = [];
+
+  $(weekSelector).each((_: number, row: CheerioElement) => {
+    weeks.push({
+      id: $(timetableWeekSelector, row).text(),
+      name: $(teachingWeekSelector, row).text(),
+      startDate: $(startDateSelector, row).text(),
+    });
+  });
+
+  return weeks;
+}
+
+export function parseModuleDetails($: CheerioStatic): IModuleDetails {
+  const idSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(2)';
+  const nameSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(2) > td:nth-child(2)';
+
+  return {
+    id: $(idSelector).text().trim(),
+    name: $(nameSelector).text().trim(),
+  };
+}
+
+export function parseModuleExamTimetable($: CheerioStatic): IModuleExamTimetable {
+  const moduleIdSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(2)';
+  const roomIdSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(6) > td:nth-child(2)';
+  const dateSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(3) > td:nth-child(2)';
+  const timeSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(7) > td:nth-child(2)';
+  const infoSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(8) > td:nth-child(2)';
+
+  return {
+    moduleId: $(moduleIdSelector).text(),
+    roomIds: parseRoomIds($(roomIdSelector).text()),
+    date: $(dateSelector).text(),
+    time: $(timeSelector).text(),
+    info: $(infoSelector).text(),
+  };
+}
+
+export function parseStudentExamTimetable($: CheerioStatic): IModuleExamTimetable[] {
+  const moduleTimetableSelector = 'body > b > table > tbody > tr > td > div > center > table > tbody';
+  const moduleIdSelector = 'tr:nth-child(1) > td:nth-child(2)';
+  const roomIdSelector = 'tr:nth-child(6) > td:nth-child(2)';
+  const dateSelector = 'tr:nth-child(3) > td:nth-child(2)';
+  const timeSelector = 'tr:nth-child(7) > td:nth-child(2)';
+  const infoSelector = 'tr:nth-child(8) > td:nth-child(2)';
+
+  const moduleExamTimetables: IModuleExamTimetable[] = [];
+
+  $(moduleTimetableSelector).each((_: number, moduleTimetable: CheerioElement) => {
+    moduleExamTimetables.push({
+      moduleId: $(moduleIdSelector, moduleTimetable).text(),
+      roomIds: parseRoomIds($(roomIdSelector, moduleTimetable).text()),
+      date: $(dateSelector, moduleTimetable).text(),
+      time: $(timeSelector, moduleTimetable).text(),
+      info: $(infoSelector, moduleTimetable).text(),
+    });
+  });
+
+  return moduleExamTimetables;
+}
+
+export function parseTimetable<T extends ITimetableLesson>($: CheerioStatic, parseLesson: (parts: string[]) => T): ITimetableLessons<T> {
+  const daySelector = 'body > div > table > tbody > tr:nth-child(2) > td';
+  const lessonSelector = 'p:not(:last-child) > font > b';
+  const lessonSplitRegex = /\s*(?:<font>.*?<\/font>|<br>)+\s*/g;
+
+  const lessons: ITimetableLessons<T> = {
+    [Day.Monday]: [],
+    [Day.Tuesday]: [],
+    [Day.Wednesday]: [],
+    [Day.Thursday]: [],
+    [Day.Friday]: [],
+    [Day.Saturday]: [],
+  };
+
+  $(daySelector).each((day: Day, timetableDay: CheerioElement) => {
+    $(lessonSelector, timetableDay).each((_: number, lesson: CheerioElement) => {
+      const lessonParts = $(lesson).html().split(lessonSplitRegex);
+      lessons[day].push(parseLesson(lessonParts));
+    });
+  });
+
+  return lessons;
+}
+
+export function parseModuleTimetableLesson(lessonParts: string[]): IModuleTimetableLesson {
+  const lessonType = LessonType[lessonParts[2]];
+
+  return {
+    fromTime: lessonParts[0],
+    toTime: lessonParts[1],
+    lessonType: lessonType,
+    group: hasGroups(lessonType) ? lessonParts[3] : null,
+    instructor: lessonParts[4],
+    roomIds: parseRoomIds(lessonParts[5]),
+    weekIds: parseWeekIds(lessonParts[6]),
+  };
+}
+
+export function parseRoomTimetableLesson(lessonParts: string[]): IRoomTimetableLesson {
+  const lessonType = LessonType[lessonParts[3]];
+
+  return {
+    fromTime: lessonParts[0],
+    toTime: lessonParts[1],
+    moduleIds: lessonParts[2].split(/\s+/g),
+    lessonType: lessonType,
+    groups: hasGroups(lessonType) ? lessonParts[4].split(/\s+/g) : null,
+    size: Number(lessonParts[5].slice(7)),
+    instructor: lessonParts[4],
+    weekIds: parseWeekIds(lessonParts[6]),
+  };
+}
+
+export function parseCourseTimetableLesson(lessonParts: string[]): ICourseTimetableLesson {
+  const lessonType = LessonType[lessonParts[3]];
+
+  return {
+    fromTime: lessonParts[0],
+    toTime: lessonParts[1],
+    moduleId: lessonParts[2],
+    lessonType: lessonType,
+    group: hasGroups(lessonType) ? lessonParts[4] : null,
+    instructor: lessonParts[5],
+    roomIds: parseRoomIds(lessonParts[6]),
+    weekIds: parseWeekIds(lessonParts[7]),
+  };
+}
+
+export function parseStudentTimetableLesson(lessonParts: string[]): IStudentTimetableLesson {
+  const lessonType = LessonType[lessonParts[3]];
+
+  return {
+    fromTime: lessonParts[0],
+    toTime: lessonParts[1],
+    moduleId: lessonParts[2],
+    lessonType: lessonType,
+    group: hasGroups(lessonType) ? lessonParts[4] : null,
+    roomIds: parseRoomIds(lessonParts[5]),
+    weekIds: parseWeekIds(lessonParts[6]),
+  };
+}
+
+export function parseHyphenatedRoomId(hyphenatedRoomId: string): string[] {
+  // Split the ID on the hyphen and get the constant prefix of the range
+  const parts = hyphenatedRoomId.split('-');
+  const prefix = parts[0].slice(0, parts[0].length - parts[1].length);
+
+  // Get the first and last numbers in the ID range
+  const startNum = Number(parts[0].slice(prefix.length));
+  const endNum = Number(parts[1]);
+
+  // Construct an array of room IDs encompassed by the range
+  const roomIds: string[] = [];
+  for (let suffix = startNum; suffix <= endNum; suffix += 1) {
+    roomIds.push(`${prefix}${suffix}`);
+  }
+
+  return roomIds;
+}
+
+export function parseRoomIds(roomIdString: string): string[] {
+  // Split on whitespace
+  const splitIds = roomIdString.trim().split(/\s+/g);
+
+  // Expand IDs where necessary
+  const roomIds: string[] = [];
+  for (const id of splitIds) {
+    if (id.includes('-')) {
+      roomIds.push(...parseHyphenatedRoomId(id));
+    } else {
+      roomIds.push(id);
+    }
+  }
+
+  return roomIds;
+}
+
+export function parseWeekIds(weekIdString: string): string[] {
+  // Returns a range of numbers as an array
+  const range = (start: number, length: number) => [...Array(length).keys()].map((x: number) => String(x + start));
+
+  // Expands an array of IDs and ID ranges into an array of IDs
+  const expandRanges = (acc: string[], curr: string) => {
+    // Expand hyphenated ID ranges
+    if (curr.includes('-')) {
+      const parts = curr.split('-');
+      const start = Math.min(Number(parts[0]), Number(parts[1]));
+      const length = Math.abs(Number(parts[0]) - Number(parts[1]));
+
+      return acc.concat(range(start, length));
+    }
+    // Don't change single IDs
+    acc.push(curr);
+
+    return acc;
+  };
+
+  // Remove prefix and split into single weeks and ranges
+  const weeks = weekIdString.slice(weekIdString.indexOf(':') + 1).split(',');
+
+  // Expand ranges
+  return weeks.reduce(expandRanges, []);
+}
+
+export function hasGroups(lessonType: LessonType): boolean {
+  return lessonType === LessonType.Laboratory || lessonType === LessonType.Tutorial;
+}
